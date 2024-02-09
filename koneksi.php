ec23290d27c104406fe61b6e17d5687b32d17a89
gemafajar09 <gemafajarramadhan09@gmail.com>
2024-02-09 17:51:05 +0700

    cors

--- koneksi.php
+++ koneksi.php
@@ -3,5 +3,10 @@
 // $conn = new mysqli("localhost",'root','','realtime_count');
 $conn = new mysqli("localhost",'mede1442_quickcount','asdasd123456789','mede1442_quickcount');
 
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
+header('Access-Control-Allow-Headers: token, Content-Type');
+header('Access-Control-Max-Age: 1728000');
+header('Content-Length: 0');
+header('Content-Type: text/plain');
+header('Content-Type: application/json');
