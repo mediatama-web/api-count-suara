6d4e147c441e5bcf0e1bc39f5a928d5cf46f4839
gemafajar09 <gemafajarramadhan09@gmail.com>
2024-02-09 17:47:54 +0700

    ok

--- koneksi.php
+++ koneksi.php
@@ -1,6 +1,7 @@
 <?php
 
-$conn = new mysqli("localhost",'root','','realtime_count');
+// $conn = new mysqli("localhost",'root','','realtime_count');
+$conn = new mysqli("localhost",'mede1442_quickcount','asdasd123456789','mede1442_quickcount');
 
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
