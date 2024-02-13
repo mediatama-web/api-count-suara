d110787cd3a9e9042189ec942c99330837d85247
gemafajar09 <gemafajarramadhan09@gmail.com>
2024-02-13 08:29:00 +0700

    main

--- login.php
+++ login.php
@@ -7,7 +7,7 @@ $post = json_decode(file_get_contents('php://input'));
 $username = $post->username;
 $password = $post->password;
 
-$user = $conn->query("SELECT * FROM tb_users WHERE username = '$username'")->fetch_assoc();
+$user = $conn->query("SELECT a.id_user, a.nama_user, a.password, a.id_kecamatan, b.nama_kecamatan FROM tb_users a LEFT JOIN tb_kecamatan b ON b.id_kecamatan=a.id_kecamatan WHERE a.username = '$username'")->fetch_assoc();
 
 if($user){
     if(password_verify($password, $user['password'])){
@@ -15,15 +15,12 @@ if($user){
         $data = array(
             'id_user' => $user['id_user'],
             'nama_user' => $user['nama_user'],
-            'id_kecamatan' => $user['id_kecamatan']
+            'id_kecamatan' => $user['id_kecamatan'],
+            'nama_kecamatan' => $user['nama_kecamatan'],
         );
     }else{
         $pesan = "password salah!";
-        $data = array(
-            'id_user' => $user['id_user'],
-            'nama_user' => $user['nama_user'],
-            'id_kecamatan' => $user['id_kecamatan']
-        );
+        $data = array();
     }
 }else{
     $pesan = "Username salah!";
